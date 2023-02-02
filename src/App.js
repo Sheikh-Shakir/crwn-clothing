e4c7f6203d871365cfd0a8eebff63661bfe04297
ShakirAhmad <shakirmca16@gmail.com>
2023-02-02 21:33:58 +0530

    updated out app with a shop page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path='/' element={<HomePage />} />
-        <Route path='/hats' element={<HatsPage />} />
+        <Route path='/shop' element={<ShopPage />} />
       </Routes>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Directory from "../../components/directory/directory.component";
 import "./homepage.style.scss";
-
 const HomePage = () => {
 	return (
 		<div className="homepage">
