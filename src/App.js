44661afdcda37e120d6a3ff2d7bee6f09b2eb783
ShakirAhmad <shakirmca16@gmail.com>
2023-02-03 22:17:55 +0530

    added state snapshot to user authentication

--- src/App.js
+++ src/App.js
@@ -10,21 +10,39 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 class App extends React.Component {
   constructor() {
     super();
+
     this.state = {
       currentUser: null
-    }
+    };
   }
+
   unsubscribeFromAuth = null;
+
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => console.log(this.state));
 
 
-    })
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
+
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
+
   render() {
     return (
       <div className="App">
