3b545a820ba0395a165e837a406d4dc5b75fc094
ShakirAhmad <shakirmca16@gmail.com>
2023-02-05 21:16:18 +0530

    Added Redux for state Management

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { applyMiddleware } from 'redux';
+// import { configureStore } from '@reduxjs/toolkit';
+import { createStore } from 'redux'
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const middlewares = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+
+export default store;
