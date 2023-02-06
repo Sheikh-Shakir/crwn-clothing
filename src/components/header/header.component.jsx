43d8ac999cd91ec87de866a1fad5a65993fea8f0
ShakirAhmad <shakirmca16@gmail.com>
2023-02-06 23:17:05 +0530

    added dropdown to cart for displaying different items in cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,10 @@ import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
 import './header.styles.scss';
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 const Header = ({ currentUser }) => {
 	return (
 		<div className="header">
@@ -14,12 +17,14 @@ const Header = ({ currentUser }) => {
 				<Link className="option" to='/shop'>SHOP</Link>
 				<Link className="option" to='/contact'>CONTACT</Link>
 				{
-					currentUser ?
+					currentUser ? (
 						<div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-						:
+					) : (
 						<Link className="option" to='/signin'>SIGN IN</Link>
-				}
+					)}
+				<CartIcon />
 			</div>
+			<CartDropdown />
 		</div>
 	)
 }
